a9a2a0c8fd9799c14566d80777f0b81fc4316bae
rioalifian12 <rioalifian12@gmail.com>
2024-12-18 16:18:50 +0700

    optimize hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,11 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// tutup hamburger dengan klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
